3df2750fd6e49e74503cbdf5dfcc9134b03df39f
Gaurav.S.Srivastava <gauravsrivastava1501@gmail.com>
2024-02-25 19:12:27 +0530

    datatypes in TS

--- dist/index.js
+++ dist/index.js
@@ -1,3 +1,13 @@
 "use strict";
-const a = 334;
-alert(a);
+let a = "not";
+let no = 45;
+let check = "Hello"; // ----> an other way of providing type to a variable
+let surname; // ---> union datatype surname can now be a string as well as a number
+surname = "yoo";
+const func = (n, m) => {
+    console.log(n, m);
+    return n * m;
+};
+console.log(a);
+console.log(no);
+console.log(surname);
--- src/index.ts
+++ src/index.ts
@@ -1,3 +1,15 @@
-const a = 334
+let a: string = "not"
+let no: number = 45
+let check = <string> "Hello"  // ----> an other way of providing type to a variable
 
-alert(a)
+let surname: string | number;  // ---> union datatype surname can now be a string as well as a number
+surname =  "yoo"
+
+const func  = (n: number, m: number) : number => {  // here we have declared that a number will be returned from this function now we cannot return string or void
+    console.log(n,m)
+    return n * m
+}
+
+console.log(a)
+console.log(no)
+console.log(surname)
