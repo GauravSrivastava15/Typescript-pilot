0a89b8dc547595d2d23b3ac6e77473b77419f91e
Gaurav.S.Srivastava <gauravsrivastava1501@gmail.com>
2024-02-27 19:47:26 +0530

    Array in TS

--- dist/index.js
+++ dist/index.js
@@ -1,13 +1,13 @@
 "use strict";
-let a = "not";
-let no = 45;
-let check = "Hello"; // ----> an other way of providing type to a variable
-let surname; // ---> union datatype surname can now be a string as well as a number
-surname = "yoo";
-const func = (n, m) => {
-    console.log(n, m);
-    return n * m;
-};
-console.log(a);
-console.log(no);
-console.log(surname);
+const arr1 = [1, 2, 3, 4, 5];
+const arr2 = ["abc", "def", "ghi"];
+const arr = new Array(20);
+const arr4 = [230, 451, "Hello"];
+const tup = [24, 45.2, 23];
+// type UserName = string | number;  // defining types, Type Aliases
+// let a:UserName = "hello"
+// type UserName = (n:string, m:string) => string
+// const func: UserName = (n,m) => {  // here we have declared that a number will be returned from this function now we cannot return string or void
+//     console.log(n,m)
+//     return n + m
+// }
--- src/index.ts
+++ src/index.ts
@@ -1,15 +1,18 @@
-let a: string = "not"
-let no: number = 45
-let check = <string> "Hello"  // ----> an other way of providing type to a variable
+const arr1: number []= [1,2,3,4,5]
+const arr2: Array<string> = ["abc","def","ghi"]
+const arr: Array<string> = new Array(20)
+const arr4: Array<string | number> = [230,451,"Hello"]
+const tup: [number,number,number] = [24,45.2,23]
 
-let surname: string | number;  // ---> union datatype surname can now be a string as well as a number
-surname =  "yoo"
 
-const func  = (n: number, m: number) : number => {  // here we have declared that a number will be returned from this function now we cannot return string or void
-    console.log(n,m)
-    return n * m
-}
 
-console.log(a)
-console.log(no)
-console.log(surname)
+
+// type UserName = string | number;  // defining types, Type Aliases
+// let a:UserName = "hello"
+
+// type UserName = (n:string, m:string) => string
+
+// const func: UserName = (n,m) => {  // here we have declared that a number will be returned from this function now we cannot return string or void
+//     console.log(n,m)
+//     return n + m
+// }
