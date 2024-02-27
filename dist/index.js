f0b712f00f720f70d4417232b61443018f093888
Gaurav.S.Srivastava <gauravsrivastava1501@gmail.com>
2024-02-27 22:53:05 +0530

    Functions in TS

--- dist/index.js
+++ dist/index.js
@@ -1,39 +1,70 @@
 "use strict";
-//use interface whenever you want to create class or object
-// interface can be extended
-const gg = {
-    height: 123,
-    weight: 34,
-    scholar: false,
-    func: (n, m) => {
-        console.log(n * m);
+const func = (n, m, l) => {
+    if (typeof l === 'undefined') {
+        return n * m;
     }
+    return n * m * l;
 };
-const npc = {
-    height: 160,
-    weight: 48,
-    scholar: true,
-    func: (n, m) => {
-        console.log(n * m);
-    }
+func(25, 23);
+// type FunctionType = (n:number, m:number, l?: number) => number
+// //default value of l
+// const fun: FunctionType = (n,m,l=20) =>{     
+//     return n*m*l;
+// }
+const fun = (n, m, l = 20) => {
+    return n * m * l;
 };
-npc.func(5, 30);
-// type Obj = {
+fun(25, 23);
+const fun1 = (...m) => {
+    return m;
+};
+fun1(23, 34, 52, 64, 75);
+// //use interface whenever you want to create class or object
+// // interface can be extended
+// interface Obj{
 //     height: number,
 //     weight: number,
 //     gender?: boolean,    // ? is used to make the property optional
 // }
-const obj = {
-    height: 125,
-    weight: 40,
-    gender: true
-};
-const obj2 = {
-    height: 145,
-    weight: 50,
-    gender: false
-};
-const obj3 = {
-    height: 135,
-    weight: 48
-};
+// type FuncType = (n: number, m: number) => void
+// interface newObj extends Obj{
+// scholar: boolean,
+// // func?: (n:number, m:number) => void
+// func: FuncType     // we have used types in interface here
+// }
+// const gg: newObj ={
+//     height: 123,
+//     weight:34,
+//     scholar: false,
+//     func: (n,m) =>{
+//         console.log(n*m)
+//     }
+// }
+// const npc: newObj = {
+//     height: 160,
+//     weight: 48,
+//     scholar: true,
+//     func: (n,m) =>{
+//         console.log(n*m)
+//     }
+// }
+// npc.func(5,30)
+// // type Obj = {
+// //     height: number,
+// //     weight: number,
+// //     gender?: boolean,    // ? is used to make the property optional
+// // }
+// const obj: Obj = {
+//     height: 125,
+//     weight: 40,
+//     gender: true
+// }
+// const obj2: Obj = {
+//     height: 145,
+//     weight: 50,
+//     gender:false
+// }
+// const obj3: Obj = {
+//     height: 135, 
+//     weight: 48
+// }
